1a804a25f8fd64cd2a2aeaa27b4c5a9b80327be7
bmartinez887 <bmartinez887@uvaq.edu.mx>
2025-10-14 20:45:50 -0600

    Add files via upload

---
+++ miprimerthema/index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="mystyle.css">
+    <link rel="stylesheet" href="../assets/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <main class="homepage">
+        <div class="container01">
+            <div class="num01">
+                <p>01</p>
+            </div>
+            <div class="project">
+                <p>PROJECT</p>
+            </div>
+            <div class="ICONIG">
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+        </div>
+        <div class="containerIMG1">
+            <img src="image.png">
+        </div>
+        <div class="container02">
+            <div class="num02">
+                <p>02</p>
+            </div>
+            <div class="project2">
+                <p>PROJECT</p>
+            </div>
+            <div class="ICONIG2">
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+        </div>
+        <div class="containerIMG2">
+            <img src="image copy.png">
+        </div>
+        <div class="containerIMG3">
+            <img src="image copy 2.png">
+        </div>
+        <div class="container03">
+            <div class="num03">
+                <p>03</p>
+            </div>
+            <div class="project3">
+                <p>PROJECT</p>
+            </div>
+            <div class="ICONIG3">
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+        </div>
+        <div class="containerIMG4">
+            <img src="image copy 3.png">
+        </div>
+        <div class="container04">
+            <p>porta non pulvinar neque labreet suspendise interdum consectetur</p>
+        </div>
+    </main>
+    <div class="slide01">
+        <div class="maintext">
+            <div class="nums01">
+                <p>01</p>
+            </div>
+            <div class="projects1">
+                <p>PROJECT</p>
+            </div>
+            <div class="ready1">
+                <p>Ready to get</p>
+            </div>
+            <div class="started1">
+                <p>started?</p>
+            </div>
+            <div class="parrafo1">
+                <p>Article evident arrived express highest men did boy.</p>
+            </div>
+            <div class="puntos1">
+                <div class="pun1">
+                    <p>jberaioufhoubfqiuvawoi</p>
+                </div>
+                <div class="pun2">
+                    <p>jberaioufhoubfqiuvawoi</p>
+                </div>
+                <div class="pun3">
+                    <p>jberaioufhoubfqiuvawoi</p>
+                </div>
+                <div class="pun4">
+                    <p>jberaioufhoubfqiuvawoi</p>
+                </div>
+            </div>
+        </div>
+        <div class="slide01IMG">
+            <img src="image.png">
+        </div>
+    </div>
+    <div class="slide02">
+        <div class="slide02IMG">
+            <img src="image copy 2.png">
+        </div>
+        <div class="maintext2">
+            <div class="nums02">
+                <p>02</p>
+            </div>
+            <div class="projects2">
+                <p>PROJECT</p>
+            </div>
+            <div class="platform">
+                <p>A platform</p>
+            </div>
+            <div class="you">
+                <p>you</p>
+            </div>
+            <div class="trust">
+                <p>can trust</p>
+            </div>
+            <div class="parrafo2">
+                <p>Article evident arrived express highest men did boy.</p>
+            </div>
+
+        </div>
+
+    </div>
+    <div class="slide03">
+        <div class="maintext3">
+            <div class="nums03">
+                <p>03</p>
+            </div>
+            <div class="projects3">
+                <p>PROJECT</p>
+            </div>
+            <div class="whole-team">
+                <p>For the whole team</p>
+            </div>
+
+            <div class="parrafo3">
+                <p>Article evident arrived express highest men did boy.</p>
+            </div>
+            <div class="puntos3">
+                <div class="pun01">
+                    <p>jberaioufhoubfqiuvawoi</p>
+                </div>
+                <div class="pun02">
+                    <p>jberaioufhoubfqiuvawoi</p>
+                </div>
+                <div class="pun03">
+                    <p>jberaioufhoubfqiuvawoi</p>
+                </div>
+                <div class="pun04">
+                    <p>jberaioufhoubfqiuvawoi</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="slide03IMG">
+            <img src="image copy 3.png">
+        </div>
+
+    </div>
+    <div class="slide04">
+        <div class="slide04IMG">
+            <img src="image copy.png">
+        </div>
+        <div class="maintext4">
+            <div class="nums04">
+                <p>04</p>
+            </div>
+            <div class="projects4">
+                <p>PROJECT</p>
+            </div>
+            <div class="no-code">
+                <p>No-code automation</p>
+            </div>
+            <div class="security">
+                <p>for security teams</p>
+            </div>
+
+            <div class="parrafo2">
+                <p>Article evident arrived express highest men did boy.</p>
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
